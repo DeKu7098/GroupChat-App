ddf7a8f8e19e3bbb52047502246b47b5f4999259
Arka Chakraborty <arkac720@gmail.com>
2023-07-20 13:56:53 +0530

    signIn backend

--- controller/userController.js
+++ controller/userController.js
@@ -1,9 +1,13 @@
 const bcrypt = require("bcrypt");
 const sequelize = require("../connect");
-const jwt=require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 const USER = require("../models/user");
 
- exports.signUp = async (req, res) => {
+function generateAccessToken(id, name) {
+  return jwt.sign({ userId: id, name: name }, process.env.TOKEN);
+}
+
+exports.signUp = async (req, res, next) => {
   try {
     const t = await sequelize.transaction();
     const name = req.body.name;
@@ -15,7 +19,7 @@ const USER = require("../models/user");
 
     if (search || search2) {
       res.status(501).json({ message: "Already a user" });
-    } else if(!search && !search2){
+    } else if (!search && !search2) {
       const saltrounds = 10;
       bcrypt.hash(password, saltrounds, async (err, hash) => {
         if (hash) {
@@ -40,4 +44,30 @@ const USER = require("../models/user");
     await t.rollback();
     res.status(500).json({ message: err });
   }
-}
+};
+
+exports.signIn = async (req, res, next) => {
+  try {
+    const email = req.body.email;
+    const password = req.body.password;
+    const search = await USER.findOne({ where: { email: email } });
+    if (search) {
+      bcrypt.compare(password, search.password, (err, result) => {
+        if (result) {
+          res
+            .status(200)
+            .json({
+              message: "logging in",
+              token: generateAccessToken(search.id, search.name)
+            });
+        } else {
+          res.status(501).json({ message: "Wrong Password" });
+        }
+      });
+    } else {
+      res.status(404).json({ message: "Account not found" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "err" });
+  }
+};
